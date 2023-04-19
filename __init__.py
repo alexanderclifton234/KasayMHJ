59aaf2562c2deb21b55672ab41a9e32c75617a0d
alexanderclifton234 <104137512+alexanderclifton234@users.noreply.github.com>
2023-04-19 19:11:53 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,11 +10,9 @@ import sys
 
 from omegaconf import OmegaConf
 
-from minigpt4.common.registry import registry
 
 from minigpt4.datasets.builders import *
 from minigpt4.models import *
-from minigpt4.processors import *
 from minigpt4.tasks import *
 
 
@@ -23,7 +21,6 @@ default_cfg = OmegaConf.load(os.path.join(root_dir, "configs/default.yaml"))
 
 registry.register_path("library_root", root_dir)
 repo_root = os.path.join(root_dir, "..")
-registry.register_path("repo_root", repo_root)
 cache_root = os.path.join(repo_root, default_cfg.env.cache_root)
 registry.register_path("cache_root", cache_root)
 
